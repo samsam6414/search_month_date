7f2b6a71ff51ea8aeeccd696726541aea40d7950
samsam6414 <99627872+samsam6414@users.noreply.github.com>
2022-02-17 13:50:20 +0800

    加入專案檔案。

---
+++ month/month.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+#define BOX_SIZE 20 
+
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+
+	int month;
+
+	cout << "Inter the number of the month" << endl;
+	cin >> month;
+
+	if (month == 1 || month == 3 || month == 5 ||
+		month == 7 || month == 8 || month == 10 ||
+
+		month == 12) {
+
+
+		cout << "The month have 31 days" << endl;
+	}
+
+	else if (month == 2) {
+
+		cout << "The month have 28 days" << endl;
+	}
+
+	else {
+
+		cout << "The month have 30 days" << endl;
+
+	}
+
+	return 0;
+}
